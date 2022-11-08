f28125431e1d994e0ebb0b7756106521ed7bc551
sirisandnes <siribsa@stud.ntnu.no>
2022-11-08 18:02:32 +0100

    "Started implementation of minimal viable product"

---
+++ src/main/java/no/ntnu/idata2304/group14/plantmonitor/MainApp.java
@@ -0,0 +1,9 @@
+package no.ntnu.idata2304.group14.plantmonitor;
+
+import no.ntnu.idata2304.group14.plantmonitor.ui.MainWindow;
+
+public class MainApp {
+    public static void main(String[] args) {
+        MainWindow.main(args);
+    }
+}
